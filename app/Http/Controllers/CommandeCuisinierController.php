af27a4fdd5e8d871d3decc3cd4c665a86d90597d
AkramChawki <akramchawki01@gmail.com>
2023-09-03 18:50:41 +0100

    fixed stuff again

--- app/Http/Controllers/CommandeCuisinierController.php
+++ app/Http/Controllers/CommandeCuisinierController.php
@@ -9,6 +9,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Mail;
 
+function generate_pdf_and_save($view, $file_name, $directory)
+{
+    $pdf = new \mikehaertl\wkhtmlto\Pdf(view($view)->render());
+    $pdf->binary = base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
+    if (!$pdf->saveAs(public_path("storage/$directory/$file_name"))) {
+        $error = $pdf->getError();
+        dd($error);
+    }
+    return asset("storage/$directory/$file_name");
+}
 class CommandeCuisinierController extends Controller
 {
     public function store(Request $request)
@@ -32,9 +42,7 @@ class CommandeCuisinierController extends Controller
         $order->detail = $detail;
         $order->save();
         $pdf_name = "Commande-".$order->created_at->format("d-m-Y")."-".$order->id.".pdf";
-        $pdf = Pdf::loadView('pdf.order-summary', compact("order"))
-        ->save(public_path("storage/documents/$pdf_name"));
-        
+        generate_pdf_and_save("pdf.order-summary", $pdf_name, "documents");
         Mail::to("admin@cucinanapoli.com")->send(new OrderSummary($order, $pdf_name));
         $order->pdf = $pdf_name;
         $order->save();
--- app/Http/Controllers/InventaireCuisinierController.php
+++ app/Http/Controllers/InventaireCuisinierController.php
@@ -9,6 +9,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Mail;
 
+function generate_pdf_and_save($view, $file_name, $directory)
+{
+    $pdf = new \mikehaertl\wkhtmlto\Pdf(view($view)->render());
+    $pdf->binary = base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
+    if (!$pdf->saveAs(public_path("storage/$directory/$file_name"))) {
+        $error = $pdf->getError();
+        dd($error);
+    }
+    return asset("storage/$directory/$file_name");
+}
 class InventaireCuisinierController extends Controller
 {
     public function store(Request $request)
@@ -32,9 +42,7 @@ class InventaireCuisinierController extends Controller
         $order->detail = $detail;
         $order->save();
         $pdf_name = "Inventaire-".$order->created_at->format("d-m-Y")."-".$order->id.".pdf";
-        $pdf = Pdf::loadView('pdf.inventaire-summary', compact("order"))
-            ->save(public_path("storage/documents/$pdf_name"));
-
+        generate_pdf_and_save("pdf.inventaire-summary", $pdf_name, "documents");
         Mail::to("admin@cucinanapoli.com")->send(new InventaireSummary($order, $pdf_name));
         $order->pdf = $pdf_name;
         $order->save();
