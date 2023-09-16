34d9b85d91a6e54e9b75265d4dd673dd7a1fc187
AkramChawki <akramchawki01@gmail.com>
2023-09-16 08:37:25 +0100

    fixed button

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -52,7 +52,7 @@
             <div class='px-4 py-4'>
                 <button type="submit"
                     class="inline-flex items-center w-[100%] mt-10 px-4 py-4 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-                    Commander
+                    Enregistrer
                 </button>
                 <a href="/"
                     class="inline-flex items-center w-[100%] mt-2 px-4 py-4 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
